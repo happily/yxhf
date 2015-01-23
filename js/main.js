2c13c971f6f0aa032425c01a1db6ddf234b8fb2c
happily <junfang@hypersun.com>
2015-01-23 17:49:45 +0800

    add html

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// JavaScript Document
+$(document).ready(function(){
+  $("#footerMenu").click(function(){
+  $(".footer-menu").toggle();
+  });
+});
+
+var lis=$(".sec-imgBox");
+	for(var i = 0;i<lis.length;i++){
+		if(i%2 == 0){
+			lis.eq(i).css({"right":"1000px"});
+			lis.eq(i).animate({"right":"0px"},200*i+1000,"swing");
+		}else{
+			lis.eq(i).css({"left":"1000px"});
+			lis.eq(i).animate({"left":"0px"},200*i+1000,"swing");
+		}
+	}
